3db043035610c4020c33aaa425aad802daa4adcb
technicakidz <y.technicakidz08@gmail.com>
2021-07-20 00:17:47 +0900

    [WIP] input の追加とルーティング作成

---
+++ vite-project/src/components/Board.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Board extends React.Component {
+  render(){
+    return(
+      <div>
+        <h1>Board</h1>
+      </div>
+    )
+  }
+}
+
+
+export default Board;
